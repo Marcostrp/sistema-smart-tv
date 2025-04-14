fd5dd953c93dd1155c08cf042a58dfd16cb4900a
Marcostrp <marcostrp@gmail.com>
2025-04-14 15:17:39 -0300

    projeto smartTv

---
+++ src/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+        
+        System.out.println ("TV está ligada? " + smartTv.ligada);
+        System.out.println ("Canal atual: " + smartTv.canal);
+        System.out.println ("Volume atual: " + smartTv.volume);
+        
+
+        smartTv.ligar();
+        System.out.println ("A TV ligou? " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("A TV desligou? " + smartTv.ligada);
+    
+    }
+}
