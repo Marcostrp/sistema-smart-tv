bc12cb8197bbdedf96c88a3d81e785d7908d2b5d
Marcostrp <marcostrp@gmail.com>
2025-04-14 16:05:50 -0300

    postando no git

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -9,5 +9,26 @@ public class SmartTv {
     public void desligar() {
         ligada = false;
     }
+    public void aumentarVolume() {
+        System.out.println("Aumentando o volume para: " + volume);
+        //volume = volume + 1;
+        volume++;
+    }
+    public void diminuirVolume() {
+        System.out.println("Diminuindo o volume para: "+ volume);
+        //volume = volume + 1;
+        volume--;
+    }
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+    public void aumentarCanal () {
+        canal++;
+        System.out.println("Mudando canal para: " + canal);
+    }
+    public void diminuirCanal (){
+        canal--;
+        System.out.println("Mudando canal para: " + canal);
+    }
 
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -2,16 +2,28 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
 
         SmartTv smartTv = new SmartTv();
-        
-        System.out.println ("TV está ligada? " + smartTv.ligada);
+
+        smartTv.mudarCanal(10);
         System.out.println ("Canal atual: " + smartTv.canal);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        
         System.out.println ("Volume atual: " + smartTv.volume);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+    
+        System.out.println ("Volume atual: " + smartTv.volume);
+        System.out.println ("TV está ligada? " + smartTv.ligada);
+       
         
 
         smartTv.ligar();
-        System.out.println ("A TV ligou? " + smartTv.ligada);
+        System.out.println ("Novo status - A TV está ligada? " + smartTv.ligada);
+        System.out.println("Novo comando!");
         smartTv.desligar();
-        System.out.println("A TV desligou? " + smartTv.ligada);
+        System.out.println("Novo status - A TV está ligada? " + smartTv.ligada);
     
     }
 }
